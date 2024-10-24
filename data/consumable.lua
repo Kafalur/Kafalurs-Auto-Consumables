c01efe0457f4e31cd3b9d6d4598a2a59d27efab9
Kafalur <jochenniedermeier@icloud.com>
2024-10-24 08:35:22 +0200

    complete rework ...

---
+++ data/consumable.lua
@@ -0,0 +1,62 @@
+-- Elixirs Tier 1
+local Consumables = {
+    t1_elixirs = {
+        {name = "Elixir of Advantage", options = "Elixir_SpeedAndLuck_1", snoid = 1841186, powerup = 1841195},
+        {name = "Elixir of Cold Resistance", options = "Elixir_ColdResist_2", snoid = 1066604, powerup = 1865979},
+        {name = "Elixir of Destruction", options = "Elixir_Destruction_1", snoid = 1841277, powerup = 1841280},
+        {name = "Elixir of Fire Resistance", options = "Elixir_FireResist_2", snoid = 1066694, powerup = 1865979},
+        {name = "Elixir of Fortitude", options = "Elixir_MaxLife_1", snoid = 1841344, powerup = 1841372},
+        {name = "Elixir of Iron Barbs", options = "Elixir_Ironbarb_1", snoid = 1060592, powerup = 1052091},
+        {name = "Elixir of Lightning Resistance", options = "Elixir_LightningResist_1", snoid = 1066495, powerup = 1060703},
+        {name = "Elixir of Poison Resistance", options = "Elixir_PoisonResist_4", snoid = 1066725, powerup = 1065983},
+        {name = "Elixir of Precision", options = "Elixir_Precision_1", snoid = 1066499, powerup = 1060735},
+        {name = "Elixir of Resourcefulness", options = "Elixir_MaxResource", snoid = 701382, powerup = 1865985},
+        {name = "Elixir of Shadow Resistance", options = "Elixir_ShadowResist_2", snoid = 1067052, powerup = 1865979},
+    },
+
+    t2_elixirs = {
+        {name = "Elixir of Advantage II", options = "Elixir_SpeedAndLuck_2", snoid = 1841197, powerup = 1842204},
+        {name = "Elixir of Cold Resistance II", options = "Elixir_ColdResist_5", snoid = 1066668, powerup = 1060718},
+        {name = "Elixir of Destruction II", options = "Elixir_Destruction_2", snoid = 1841287, powerup = 1842535},
+        {name = "Elixir of Fire Resistance II", options = "Elixir_FireResist_5", snoid = 1066704, powerup = 1060713},
+        {name = "Elixir of Fortitude II", options = "Elixir_MaxLife_2", snoid = 1841327, powerup = 1841375},
+        {name = "Elixir of Iron Barbs II", options = "Elixir_Ironbarb_5", snoid = 1060634, powerup = 1060631},
+        {name = "Elixir of Lightning Resistance II", options = "Elixir_LightningResist_5", snoid = 1066718, powerup = 1060722},
+        {name = "Elixir of Poison Resistance II", options = "Elixir_PoisonResist_5", snoid = 1066727, powerup = 1060726},
+        {name = "Elixir of Precision II", options = "Elixir_Precision_5", snoid = 1066737, powerup = 1062886},
+        {name = "Elixir of Resourcefulness II", options = "Elixir_Resources_2", snoid = 1841178, powerup = 1844960},
+        {name = "Elixir of Shadow Resistance II", options = "Elixir_ShadowResist_5", snoid = 1067122, powerup = 1060730},
+    },
+
+    t1_incense = {
+        {name = "Ancient Times", options = "Incense_I_04", snoid = 732663, powerup = 732664},
+        {name = "Blessed Guide", options = "Incense_I_07", snoid = 732731, powerup = 732732},
+        {name = "Desert Escape", options = "Incense_I_01", snoid = 730692, powerup = 730693},
+        {name = "Queen's Supreme", options = "Incense_I_06", snoid = 732725, powerup = 732726},
+        {name = "Sage's Whisper", options = "Incense_I_05", snoid = 732722, powerup = 732723},
+        {name = "Song of the Mountain", options = "Incense_I_02", snoid = 732604, powerup = 732606},
+        {name = "Spirit Dance", options = "Incense_I_03", snoid = 732611, powerup = 732612},
+        {name = "Storm of the Wilds", options = "Incense_I_00", snoid = 725795, powerup = 727760},
+    },
+
+    t2_incense = {
+        {name = "Reddamine Buzz", options = "Incense_II_00", snoid = 731557, powerup = 731558},
+        {name = "Scents of the Desert After", options = "Incense_II_03", snoid = 732762, powerup = 732763},
+        {name = "Soothing Spices", options = "Incense_II_01", snoid = 731614, powerup = 731616},
+        {name = "Spiral Morning", options = "Incense_II_02", snoid = 732758, powerup = 732760},
+    },
+
+    t3_incense = {
+        {name = "Chorus of War", options = "Incense_III_00", snoid = 731872, powerup = 731873},
+    },
+
+    opals = {
+        {name = "Seething Opal of Equipment", options = "S06_Realmwalker_ConsumableBuff_01", snoid = 2072996, powerup = 2040680},
+        {name = "Seething Opal of Gold", options = "S06_Realmwalker_ConsumableBuff_00", snoid = 2072959, powerup = 2042766},
+        {name = "Seething Opal of Materials", options = "S06_Realmwalker_ConsumableBuff_03", snoid = 2073000, powerup = 2042755},
+        {name = "Seething Opal of Socketed", options = "S06_Realmwalker_ConsumableBuff_02", snoid = 2072998, powerup = 2042755},
+        {name = "Seething Opal of Torment", options = "S06_Realmwalker_ConsumableBuff_04", snoid = 2073002, powerup = 2072988},
+    }
+}
+
+return Consumables
