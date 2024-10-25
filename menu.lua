65b186cf1b3f43c617df15c7ecb6253074d3d9b4
Kafalur <jochenniedermeier@icloud.com>
2024-10-25 09:51:17 +0200

    reconfigured menu
    - Menu now is always shown
    - "Enable" Toggle has clearer name "Enable Plugin"
    - menu trees implemented
    - combotext is not exceeding combobox
    - toggle above combo
    - resortet Menuorder Elixir, Incence, Opal

--- menu.lua
+++ menu.lua
@@ -11,6 +11,14 @@ local function extract_names(consumable_table)
     return names
 end
 
+local function remove_prefix(names, prefix)
+    local result = {}
+    for _, name in ipairs(names) do
+        result[#result + 1] = name:gsub("^" .. prefix, "")
+    end
+    return result
+end
+
 menu.elements = {
   main_tree = tree_node:new(0),
   main_toggle = checkbox:new(false, get_hash(plugin_label .. "_main_toggle")),
@@ -19,58 +27,60 @@ menu.elements = {
   elixir_tree = tree_node:new(1),
   incense_tree = tree_node:new(1),
 
-  opal_combo = combo_box:new(0, get_hash(plugin_label .. "_opal_combo")),
   opal_toggle = checkbox:new(false, get_hash(plugin_label .. "_opal_toggle")),
+  opal_combo = combo_box:new(0, get_hash(plugin_label .. "_opal_combo")),
 
-  t1_elixir_combo = combo_box:new(0, get_hash(plugin_label .. "_t1_elixir_combo")),
   t1_elixir_toggle = checkbox:new(false, get_hash(plugin_label .. "_t1_elixir_toggle")),
+  t1_elixir_combo = combo_box:new(0, get_hash(plugin_label .. "_t1_elixir_combo")),
 
-  t2_elixir_combo = combo_box:new(0, get_hash(plugin_label .. "_t2_elixir_combo")),
   t2_elixir_toggle = checkbox:new(false, get_hash(plugin_label .. "_t2_elixir_toggle")),
+  t2_elixir_combo = combo_box:new(0, get_hash(plugin_label .. "_t2_elixir_combo")),
 
-  t1_incense_combo = combo_box:new(0, get_hash(plugin_label .. "_t1_incense_combo")),
   t1_incense_toggle = checkbox:new(false, get_hash(plugin_label .. "_t1_incense_toggle")),
+  t1_incense_combo = combo_box:new(0, get_hash(plugin_label .. "_t1_incense_combo")),
 
-  t2_incense_combo = combo_box:new(0, get_hash(plugin_label .. "_t2_incense_combo")),
   t2_incense_toggle = checkbox:new(false, get_hash(plugin_label .. "_t2_incense_toggle")),
+  t2_incense_combo = combo_box:new(0, get_hash(plugin_label .. "_t2_incense_combo")),
 
-  t3_incense_combo = combo_box:new(0, get_hash(plugin_label .. "_t3_incense_combo")),
   t3_incense_toggle = checkbox:new(false, get_hash(plugin_label .. "_t3_incense_toggle")),
+  t3_incense_combo = combo_box:new(0, get_hash(plugin_label .. "_t3_incense_combo")),
 }
 
 function menu.render()
-  if not menu.elements.main_tree:push("KafalursAuto Consumables") then
+  if not menu.elements.main_tree:push("Kafalurs Auto Consumables") then
     return
   end
 
-  menu.elements.main_toggle:render("Enable", "Toggles Potion Buff on/off")
-  if not menu.elements.main_toggle:get() then
-    menu.elements.main_tree:pop()
-    return
-  end
+  menu.elements.main_toggle:render("Enable Plugin", "Toggles Potion Buff on/off")
 
-  if menu.elements.opal_tree:push("Opals") then
-    menu.elements.opal_combo:render("Opals", extract_names(consumables.opals), "Which opal do you want to use?")
-    menu.elements.opal_toggle:render("Opal Toggle", "Toggles Opal Potions on/off")
-    menu.elements.opal_tree:pop()
-  end
-
+  -- Elixirs
   if menu.elements.elixir_tree:push("Elixirs") then
-    menu.elements.t1_elixir_combo:render("T1 Elixirs", extract_names(consumables.t1_elixirs), "Which T1 elixir do you want to use?")
     menu.elements.t1_elixir_toggle:render("T1 Elixir Toggle", "Toggles T1 Elixir Potions on/off")
-    menu.elements.t2_elixir_combo:render("T2 Elixirs", extract_names(consumables.t2_elixirs), "Which T2 elixir do you want to use?")
+    menu.elements.t1_elixir_combo:render("T1 Elixirs", remove_prefix(extract_names(consumables.t1_elixirs), "Elixir of "), "Which T1 elixir do you want to use?")
+    
     menu.elements.t2_elixir_toggle:render("T2 Elixir Toggle", "Toggles T2 Elixir Potions on/off")
+    menu.elements.t2_elixir_combo:render("T2 Elixirs", remove_prefix(extract_names(consumables.t2_elixirs), "Elixir of "), "Which T2 elixir do you want to use?")
     menu.elements.elixir_tree:pop()
   end
 
+  -- Incenses
   if menu.elements.incense_tree:push("Incenses") then
-    menu.elements.t1_incense_combo:render("T1 Incenses", extract_names(consumables.t1_incense), "Which T1 incense do you want to use?")
     menu.elements.t1_incense_toggle:render("T1 Incense Toggle", "Toggles T1 Incenses on/off")
-    menu.elements.t2_incense_combo:render("T2 Incenses", extract_names(consumables.t2_incense), "Which T2 incense do you want to use?")
+    menu.elements.t1_incense_combo:render("T1 Incenses", extract_names(consumables.t1_incense), "Which T1 incense do you want to use?")
+    
     menu.elements.t2_incense_toggle:render("T2 Incense Toggle", "Toggles T2 Incenses on/off")
-    menu.elements.t3_incense_combo:render("T3 Incenses", extract_names(consumables.t3_incense), "Which T3 incense do you want to use?")
+    menu.elements.t2_incense_combo:render("T2 Incenses", extract_names(consumables.t2_incense), "Which T2 incense do you want to use?")
+  
     menu.elements.t3_incense_toggle:render("T3 Incense Toggle", "Toggles T3 Incenses on/off")
+    menu.elements.t3_incense_combo:render("T3 Incenses", extract_names(consumables.t3_incense), "Which T3 incense do you want to use?")
     menu.elements.incense_tree:pop()
+  end
+
+  -- Opals
+  if menu.elements.opal_tree:push("Opals") then
+    menu.elements.opal_toggle:render("Opal Toggle", "Toggles Opal Potions on/off")
+    menu.elements.opal_combo:render("Opals", remove_prefix(extract_names(consumables.opals), "Seething Opal of "), "Which opal do you want to use?")
+    menu.elements.opal_tree:pop()
   end
 
   menu.elements.main_tree:pop()
